9b211bde8726715b22655fa776ec72b6456a4acd
Antoine Drouhin <antoine.drouhin@gmail.com>
2016-11-30 20:35:37 +0100

    module files at root

--- src/module.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('eklabs.angularStarterPack',[
-    'eklabs.angularStarterPack.jsonEditor',
-    'eklabs.angularStarterPack.forms',
-    'eklabs.angularStarterPack.ttcJsonAdmin',
-    'eklabs.angularStarterPack.ttcTchatRequester'
-]);
